dec9fde75eb823429f69ff00a507d832bf985976
FirdainiAzmi <firdaini.23071@mhs.unesa.ac.id>
2025-12-11 08:34:46 +0700

    Update InstaMon.py

--- InstaMon.py
+++ InstaMon.py
@@ -105,9 +105,10 @@ with tab2:
         st.components.v1.iframe(
             src=LOOKER_EMBED_URL,
             width=1400,
-            height=620,
+            height=630,
             scrolling=True
         )
+
 
 
 
